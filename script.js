fb0b5fadeaf65235501e57385859c3b8e8f14206
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2026-01-19 22:27:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+
+function majorityElement(arr){
+	let majorityTimes=Math.floor(arr.length/2);
+	let newMap=new Map()
+
+	for(let num of arr){
+		nemMap.set(num,(newMap.get(num)+1)||0)
+	}
+
+	for(let [key,value] of newMap){
+		if(value>majorityTimes){
+			return key
+		}
+	}
+}
+
+majorityElement([2,1,2])
